f6a9db0cf7128a560b031d7eb124b3049bec0eee
Pier Carlo Chiodi <pierky@pierky.com>
2015-05-14 19:28:31 +0200

    Added some custom errors

--- Errors.py
+++ Errors.py
@@ -1,2 +1,12 @@
-class TLDsListUnavailableError(Exception):
-  pass
+class TLDsListUnavailableError(Exception): pass
+
+class ExistingConfigError(ValueError): pass
+
+class InsufficientPermissionError(Exception): pass
+
+try:
+  FileNotFoundError
+except:
+  class FileNotFoundError(Exception): pass
+
+class BuildUnchangedConfigFileError(Exception): pass
